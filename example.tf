aac9fa6cfc4b560b686bddfd118e52fda41c455e
Nathaniel Beckstead <becksteadn@gmail.com>
2019-08-11 11:31:19 -0400

    allow egress traffic

--- example.tf
+++ example.tf
@@ -83,6 +83,14 @@ resource "aws_security_group" "allow_smtp" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
 }
 
 resource "aws_instance" "spamcap" {
